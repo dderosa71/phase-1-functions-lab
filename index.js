55b78e67a1b944eab7b00c62c8782fd56b717a65
Dominic DeRosa <dderosa71@gmail.com>
2021-07-14 09:56:11 -0500

    commit tree a9dcf7290726d7de2ae32032d1baa5cff5a1c7fd

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(x){
+    const start = 42;
+    return Math.abs(x - 42 )
+}
+function distanceFromHqInFeet(x){
+    return distanceFromHqInBlocks(x) * 264   
+}
+
+function distanceTravelledInFeet(x,y){
+    return Math.abs((y - x) * 264)
+}
+function calculatesFarePrice(start, destination){
+    const feetTravelled = distanceTravelledInFeet(start,destination);  
+    if (feetTravelled < 401){
+        return 0
+    } else if ( feetTravelled < 2001){
+        return .02 * (feetTravelled - 400)
+    }
+    else if (feetTravelled <2501){
+        return 25
+    }
+    else {
+        return 'cannot travel that far'
+    }
+}
